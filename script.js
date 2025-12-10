0cad5b386e6f09c874daa1b1aa200517f523a53d
KngPik <jon.jentsch@gmail.com>
2025-12-10 16:05:13 +0100

    started work on custom unit selection

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+var valueUnitOptionElements = []
+var outputUnitOptionElements = []
+
+function initElements() {
+
+    for (let i = 1; i < 4; i++) {
+        valueUnitOptionElements.push(document.getElementById("cso-v-" + i.toString()));
+    }
+
+    for (let i = 1; i < 4; i++) {
+        outputUnitOptionElements.push(document.getElementById("cso-o-" + i.toString()));
+    }
+
+}
+
 function calculateOutput() {
     // get all relevant stakeholders
     value = document.getElementById("value").value;
-    valueUnit = document.getElementById("value-unit").value;
     scale = document.getElementById("scale").value;
-    outputUnit = document.getElementById("output-unit").value;
 
     outputText = document.getElementById("output");
 
@@ -13,3 +26,15 @@ function calculateOutput() {
     // output the result
     outputText.value = result;
 }
+
+function changeUnitSelection(id) {
+
+    valueUnitOptionElements.forEach(e => {
+        e.classList.remove("selected");
+    });
+
+    document.getElementById(id).classList.add("selected");
+
+}
+
+initElements();
