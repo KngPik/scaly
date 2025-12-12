09ba3c237cf46eeccaf1a5c288551575f444a115
KngPik <jon.jentsch@gmail.com>
2025-12-12 01:10:38 +0100

    added multiple things

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-var valueUnitOptionElements = []
-var outputUnitOptionElements = []
+class UnitOptionElement {
+
+    htmlElement;
+    unitAsString;
+    isSelected = false;
+
+    UnitElement(htmlElement, unitAsString, isSelected) {
+
+        this.htmlElement = htmlElement;
+        this.unitAsString = unitAsString;
+        this.isSelected = isSelected;
+    }
+
+    getValue() {
+
+        if (this.unitAsString == "mm") return 1;
+        if (this.unitAsString == "cm") return 10;
+        if (this.unitAsString == "m") return 1000;
+        if (this.unitAsString == "km") return 1000000;
+    }
+
+    changeSelectionStatus(newStatus) {
+
+        this.isSelected = newStatus;
+    }
+
+}
+
+var valueUnitOptionElements = [];
+var outputUnitOptionElements = [];
+
+var valueUnit = 0;
+var outputUnit = 0;
 
 function initElements() {
 
-    for (let i = 1; i < 4; i++) {
-        valueUnitOptionElements.push(document.getElementById("cso-v-" + i.toString()));
+    for (let i = 1; i < 5; i++) {
+
+        var htmlElement = document.getElementById("cso-v-" + i.toString());
+        var isSelected = htmlElement.classList.contains("selected");
+        var newValueUnitOptionElement = UnitOptionElement(htmlElement, htmlElement.innerHTML, isSelected);
+        valueUnitOptionElements.push(newValueUnitOptionElement);
     }
 
-    for (let i = 1; i < 4; i++) {
-        outputUnitOptionElements.push(document.getElementById("cso-o-" + i.toString()));
+    for (let i = 1; i < 5; i++) {
+
+        var htmlElement = document.getElementById("cso-o-" + i.toString());
+        var isSelected = htmlElement.classList.contains("selected");
+        var newOutputUnitOptionElement = UnitOptionElement(htmlElement, htmlElement.innerHTML, isSelected);
+        outputUnitOptionElements.push(newOutputUnitOptionElement);
     }
 
+    getUnits();
 }
 
 function calculateOutput() {
@@ -27,7 +67,7 @@ function calculateOutput() {
     outputText.value = result;
 }
 
-function changeUnitSelection(id) {
+function changeValueUnitSelection(id) {
 
     valueUnitOptionElements.forEach(e => {
         e.classList.remove("selected");
@@ -35,6 +75,33 @@ function changeUnitSelection(id) {
 
     document.getElementById(id).classList.add("selected");
 
+    getUnits();
+}
+
+function changeOutputUnitSelection(id) {
+
+    outputUnitOptionElements.forEach(e => {
+        e.classList.remove("selected");
+    });
+
+    document.getElementById(id).classList.add("selected");
+
+    getUnits();
+}
+
+function getUnits() {
+
+    valueUnitOptionElements.forEach(e => {
+        
+        if (e.isSelected) valueUnit = e.getValue();
+    });
+
+    outputUnitOptionElements.forEach(e => {
+        
+        if (e.isSelected) outputUnit = e.getValue();
+    });
+
+    calculateOutput();
 }
 
 initElements();
